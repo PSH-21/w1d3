2d669e3b169fbeb5fe32677254686e09edb91463
Peter Hunt <peter.s.hunt@gmail.com>
2017-09-07 04:30:44 +0000

    indexing using loop call back and forEach call back

--- w1d4/where-waldo-index.js
+++ w1d4/where-waldo-index.js
@@ -16,9 +16,9 @@ findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
 */
 
 function findWaldo(arr, found) {
-  arr.forEach(function(name) {
+  arr.forEach(function(name, index) {
     if (name === "Waldo") {
-      found(arr.indexOf(name));   // execute callback
+      found(index);   // execute callback
     }
   });
 }
@@ -28,3 +28,28 @@ function actionWhenFound(index) {
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+function forEvery(list, cb) {
+  for (var i = 0; i < list.length; i++) {
+    cb(list[i], i)
+  }
+}
+
+/*
+forEvery([1, 2, 3], function(item, index) {
+  console.log(`${item} at index ${index}`);
+});
+*/
+
+function reverse(s) {
+  for (var i = s.length - 1, o = ''; i >= 0; o += s[i--]) { }
+  return o;
+}
+
+function reverse(s) {
+  var o = '';
+  for (var i = s.length - 1; i >= 0; i--) {
+    o += s[i];
+  }
+  return o;
+}
